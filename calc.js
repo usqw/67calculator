2f87e95fb77259c72c8dba422428d89f260a62af
usqw <a.wantwit@gmail.com>
2026-07-22 20:34:57 +0800

    add feature 67 cat that stops calculator from working

--- calc.js
+++ calc.js
@@ -1,4 +1,7 @@
 var Continue = 1
+let resultDiv = document.getElementById('display');
+let meme = document.getElementById('meme')
+
    //operate on __ stages : 
     //once false 
     //// take input n store as operator 
@@ -9,14 +12,17 @@ var Continue = 1
     //calc() if statements to call on respective functions 
 
 function displayValue(a) {
-    const resultDiv = document.getElementById('display');
     const newNumber = document.createElement("p");
     newNumber.textContent = a;
     resultDiv.appendChild(newNumber);
+    sixSeven();
+    
 }
 
-let currentNum = ""; 
+let currentNum = ""; // why without wld it cause NaN
 let num1; 
+let num2;
+let operator;
 
 function storeValue(n) {
     currentNum += n;
@@ -24,8 +30,9 @@ function storeValue(n) {
 
 function operateOn(value){  
     if (Continue === 2) {
+        operator = value;
         displayValue(value);   
-        num1 = currentNum;
+        num1 = parseInt(currentNum);
         //cannot put return before naything since it exits early 
         //error let currentNum = null since will only look at enclosed n currentNum wld not be initialised yet 
         currentNum = "";
@@ -35,22 +42,44 @@ function operateOn(value){
         storeValue(value);
         displayValue(value);
         }} 
+       
 
-function operatorChoice(value) {
-    const operator = value;
-        return operator;
-}
-
-function A(value) {
+function equalsTo(value) {
     if (Continue === 3) {
         displayValue(value);
-        const num2 = currentNum;
-        console.log(num1); // see if num1 can be put in here cant 
+        num2 = parseInt(currentNum);
+        calc(num1 , num2); // see if num1 can be put in here cant 
     } else {
         operateOn(value)
     }
 
 }
+
+function calc(num1 , num2){
+    if (operator === "+") {
+        displayValue(add(num1 , num2))
+    }
+}
+
+
+function sixSeven(){
+    let pElements = resultDiv.querySelectorAll('p');
+        
+    for (let i=0 ; i<pElements.length - 1; i++) { //-1 boundary protect (i+1)
+        const firstNum = pElements[i].textContent;
+        const secondNum = pElements[i+1].textContent;
+            if (firstNum === "6" && secondNum === "7"){
+                meme.classList.add('active')
+            } 
+        }
+    for (let i=0 ; i<pElements.length; i++) {
+            if (pElements[i].textContent === "67") {
+                meme.classList.add('active')
+            }
+        }
+            
+    
+    }
 
 
 
@@ -61,18 +90,24 @@ function A(value) {
 
 
 const One = document.querySelector('.One')
-One.addEventListener ('click' , function() {A(1)} )
+One.addEventListener ('click' , function() {equalsTo(1)} )
 
 const Two = document.querySelector('.Two')
-Two.addEventListener ('click' , function() {A(2)} )
+Two.addEventListener ('click' , function() {equalsTo(2)} )
+
+const Six = document.querySelector('.Six')
+Six.addEventListener ('click' , function() {equalsTo(6)} )
+
+const Seven = document.querySelector('.Seven')
+Seven.addEventListener ('click' , function() {equalsTo(7)} )
 
 const Plus = document.querySelector('.Plus')
 Plus.addEventListener ('click' , () => {Continue = 2})
-Plus.addEventListener ('click' , function() {A('+')} )
+Plus.addEventListener ('click' , function() {equalsTo('+')} )
 
 const Equal = document.querySelector('.Equal')
 Equal.addEventListener ('click' , () => {Continue = 3})
-Equal.addEventListener ('click' , function() {A('=')} )
+Equal.addEventListener ('click' , function() {equalsTo('=')} )
 
 
 
